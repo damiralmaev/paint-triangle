503cefd447335064a88453af439ee44d6b026ed2
Gladi1 <49594144+Gladi1@users.noreply.github.com>
2019-04-25 14:21:11 +0400

    2
    да я знаю

--- рисовалка/Form1.Designer.cs
+++ рисовалка/Form1.Designer.cs
@@ -43,10 +43,6 @@
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.textBox6 = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.textBox8 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -180,49 +176,11 @@
             this.textBox6.TabIndex = 13;
             this.textBox6.Text = "0";
             // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(867, 337);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(18, 13);
-            this.label7.TabIndex = 16;
-            this.label7.Text = "x4";
-            // 
-            // textBox7
-            // 
-            this.textBox7.Location = new System.Drawing.Point(902, 337);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 20);
-            this.textBox7.TabIndex = 15;
-            this.textBox7.Text = "0";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(868, 377);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(18, 13);
-            this.label8.TabIndex = 18;
-            this.label8.Text = "y5";
-            // 
-            // textBox8
-            // 
-            this.textBox8.Location = new System.Drawing.Point(902, 374);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(100, 20);
-            this.textBox8.TabIndex = 17;
-            this.textBox8.Text = "0";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1242, 747);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.textBox7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.label5);
@@ -240,6 +198,7 @@
             this.Controls.Add(this.xandy);
             this.Name = "Form1";
             this.Text = "рисовалка";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -263,10 +222,6 @@
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox textBox8;
     }
 }
 
--- рисовалка/Form1.cs
+++ рисовалка/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Библиотеки
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,42 +9,73 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//Автор Дамир Алмаев Маратович
+
+//Сама прога
 namespace рисовалка
 {
     public partial class Form1 : Form
     {
+        //переменные
 
-        public static int x1, x2, x3, x4, y1, y2, y3, y4 = 0;
-        public static string text = "";
+        public static int x1, x2, x3, x4, y1, y2, y3, y4 = 0;//Для рисования
 
-        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        //Мусор
+
+        private void Form1_Load(object sender, EventArgs e)//При загрузки
         {
-            e.Graphics.DrawLine(System.Drawing.Pens.Red, x1, y1, x2, y2);
-            e.Graphics.DrawLine(System.Drawing.Pens.Red, x2, y2, x3, y3);
-            e.Graphics.DrawLine(System.Drawing.Pens.Red, x1, y1, x3, y3);
+            //тут нет нечего)))
         }
 
-        public Form1()
+        //Мусор
+
+        //Автор Дамир Алмаев Маратович
+
+        public static string text = "";//Просто переменная которое исправляет один баг
+
+        //Начинаем писать код!
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)//Рисование
         {
-            InitializeComponent();
+            e.Graphics.DrawLine(System.Drawing.Pens.Red, x1, y1, x2, y2);//Рисуем линию
+            e.Graphics.DrawLine(System.Drawing.Pens.Red, x2, y2, x3, y3);//Рисуем линию
+            e.Graphics.DrawLine(System.Drawing.Pens.Red, x1, y1, x3, y3);//Рисуем линию
+            //и получаем!
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        //Автор Дамир Алмаев Маратович
+
+        public Form1()//Загружаем форму
         {
-            try
+            InitializeComponent();//Загружаем форму
+        }
+
+        private void button1_Click(object sender, EventArgs e)//Когда нажимаем на кнопку, то
+        {
+            try//обработка
             {
-                x1 = Convert.ToInt32(textBox1.Text);
-                y1 = Convert.ToInt32(textBox2.Text);
-                x2 = Convert.ToInt32(textBox3.Text);
-                y2 = Convert.ToInt32(textBox4.Text);
-                x3 = Convert.ToInt32(textBox5.Text);
-                y3 = Convert.ToInt32(textBox6.Text);
-                if (text == "")
-                    text = xandy.Text;
-                xandy.Text = Convert.ToString("x1 = " + x1 + " y1 = " + y1 + " x2 = " + x2 + " y2 = " + y2 + " x3 = " + x3 + " y3 = " + y3 + " x4 = " + x4 + " y4 = " + y4);
-                pictureBox1.Refresh();
+                x1 = Convert.ToInt32(textBox1.Text);//Водим переменные инфу
+                y1 = Convert.ToInt32(textBox2.Text);//Водим переменные инфу
+                x2 = Convert.ToInt32(textBox3.Text);//Водим переменные инфу
+                y2 = Convert.ToInt32(textBox4.Text);//Водим переменные инфу
+                x3 = Convert.ToInt32(textBox5.Text);//Водим переменные инфу
+                y3 = Convert.ToInt32(textBox6.Text);//Водим переменные инфу
+
+                //Автор Дамир Алмаев Маратович
+
+                //Проверка
+                if (text == "")//если переменная text = нечего, то
+                    text = xandy.Text;//он сохраняет значении инфы
+                xandy.Text = Convert.ToString("x1 = " + x1 + " y1 = " + y1 + " x2 = " + x2 + " y2 = " + y2 + " x3 = " + x3 + " y3 = " + y3);//Выводим инфу
+                pictureBox1.Refresh();//Запускаем рестарт и рисование
             }
-            catch { MessageBox.Show("Error", "" + this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error); }
+            //Если ошибка, то
+            catch { MessageBox.Show("Error", "" + this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error); }//Выводим ошибку
         }
+
+        //Автор Дамир Алмаев Маратович
+
     }
 }
+//конец
+//Автор Дамир Алмаев Маратович
